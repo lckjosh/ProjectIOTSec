9ed80595e231ddd74dd98f8ccf1fbaf7e7903f62
j4mj4m <jamesteo2701@gmail.com>
2021-01-21 16:40:35 +0800

    Things i did in Post-Exploitation Branch: - Request user to input the baseline HTML report file. If file exists, proceed with Post-Exploitation Open Port detection. Else, print an error message to console. - In Main Menus, changed Option 2 "Help" to "Post Exploitation Scan". - Use bs4 to perform Web Scraping on the baseline HTML report file, and populate a list of dictionary (baseline_list) that contains the IPs and its ports found. - Run masscan and request user for inputs for the masscan options (tweaked the prefix and outdir default values for this post-exploitation-scan)   ~ Creates a "post-exploitation-results/" folder.   ~ Prefix starts with "post-exploitation-scan". - Parse the masscan result and store it into the ScanDB SQLite database - Retrieve all the IPs and its ports from the SQLite database and populate another list of dictionary (post-exploitation-list) - From the two unordered list of dictionary (baseline_list and post_exploitation_list), filter through all the IPs in post-exploitation-list found in baseline list that has new ports detected. As baseline_list and post_exploitation_list is an unordered list, cannot simply use built-in methods for comparison between the unordered list of dictionary. - When comparing the specific IP's for new ports detected, the value for the key "Port" is a list of strings. Hence to find new values, I have to convert both lists to sets, and use the set operation (difference) to find the set of all values of  post_exploitation_list Ports that are not values of baseline_list Ports. Then, I had to convert the result set into list (for report generation). - In projectiotsec.py, the Masscan_Parser would read the content of the text file generated by masscan to populate the SQLite database. As such, I could not remove the creation of text file. Instead, I decided to write a commnd to delete this text file when user quits the progam (before generating the HTML report) for both Masscan and Post Exploitation Scan. Hence, this text file serves as a temporary medium to populate the SQLite database. - In "VeraEdge_CVE_2019_15498.py", I added a command to remove the index.html file generated. - In "VeraEdge_CVE_2019_13598.py", I fixed a bug for the input of victim_ip - Updated .gitignore file to not track the folder "post-exploitation-results"
    Report Generation:
    - Added ‘DCS-933L’ and ‘dlink’ in iotDetectionKeyword.txt
    - In "Utils.py", added imports and create_report function
    - In "projectiotsec.py", added imports and tweaked line 298 onwards for calling the CreateReport function
    - Changed the try/except to cover the initial loop print device menu as well.
    - Create "templates" folder with logo1.png, network_scan_report_template.html & post_exploitation_scan_report_template.html
    - rename the function name to create_report (to be consistent with other function naming convention)
    - in create_report function, the else print statement I change the content from "report list" to "report_list"
    - tweak the create_report() function to pass another argument "templateName"
    - network_scan_report_template.html will only generate the Exploit Results Section if a user attempt to exploit / bruteforced a device from the Network Scan.
    - post_exploitation_scan_report_template.html will only be generated if there is a new open port found when compared against the baseline HTML
    
    Exploit Module (VeraEdge):
    - In "VeraEdge_CVE_2019_15498.py", I added a command to remove the index.html file generated.
    - In "VeraEdge_CVE_2019_13598.py", I fixed a bug for the input of victim_ip

--- projectiotsec.py
+++ projectiotsec.py
@@ -4,6 +4,7 @@ import sys
 import os
 import resources
 import re
+from bs4 import BeautifulSoup
 from exploits.HomeController.VeraEdge_CVE_2019_13598 import VeraEdge_CVE_2019_13598
 from exploits.HomeController.VeraEdge_CVE_2019_15498 import VeraEdge_CVE_2019_15498
 from exploits.IP_Camera.Foscam_C2_CVE_2018_19070 import Foscam_C2_CVE_2018_19070
@@ -16,6 +17,9 @@ from parsers import Masscan_Parser_Class
 from databases import DataBase_Class
 from Utils import *
 import ast
+from jinja2 import Environment, FileSystemLoader
+import codecs
+import time
 
 def iot_guess(portlist, hostlist):
     """
@@ -83,7 +87,7 @@ if __name__ == '__main__':
 
     # menu
     print("1. Scan Network")
-    print("2. Help")
+    print("2. Post Exploitation Scan")
     print("3. Exit")
     choice = input("\nPlease choose option number: ")
 
@@ -200,118 +204,293 @@ if __name__ == '__main__':
                 report_list.append(report_dict_copy)
 
             previous_ip = row[0]
+            
+        # set template_name for network scan HTML report
+        template_name = 'network_scan_report_template.html'
+        try:
+            # Loop for devices menu to allow user to continuously exploit multiple devices
+            while True:
+                print('\nList of all records found:\n')
+                print('1. IP = ' + first_ip)
 
-        # Loop for devices menu to allow user to continuously exploit multiple devices
-        while True:
-            print('\nList of all records found:\n')
-            print('1. IP = ' + first_ip)
-            report_dict["IP"].append(first_ip)
+                counter = 1
+                previous_ip = ''
 
-            counter = 1
-            previous_ip = ''
+                for row in rows:
+                    # counter += 1
+                    ip = ''
+                    port = ''
 
-            for row in rows:
-                # counter += 1
-                ip = ''
-                port = ''
+                    for key in row.keys():
+                        if key == 'IP':
+                            ip = row[key]
+                            if ip == first_ip:
+                                pass
 
-                for key in row.keys():
-                    if key == 'IP':
-                        ip = row[key]
-                        if ip == first_ip:
-                            pass
+                            elif ip != previous_ip:
+                                counter += 1
+                                print('\n%s. %s = %s' % (counter, key, row[key]))
 
-                        elif ip != previous_ip:
-                            counter += 1
-                            print('\n%s. %s = %s' % (counter, key, row[key]))
+                        else:
+                            port = row[key]
+                            print('   %s = %s' % (key, row[key]))
+
+                            for text in final_list:
+                                if (' ' + ip + ' ') in text:
+                                    if (' ' + port + ' ') in text:
+                                        print('   ' + text)
+
+                    previous_ip = row[0]
+
+                print('\nTotal result: '+str(counter))
+
+                while True:
+                    # ask for ip to exploit
+                    exploit_ip = input("\nPlease enter the IP address to exploit: ")
+                    regex = '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+                    r = re.compile(regex)
+                    if r.match(exploit_ip):
+                        break
+                    else:
+                        print("Invalid IP address! ")
+
+                print(exploit_ip + " may be compatible with the following exploits: \n")
+
+                # try to get categories of exploits selected ip may be compatible with
+                categories = []
+                for ip in report_list:
+                    if ip['IP'][0] == exploit_ip:
+                        for line in ip['Banner']:
+                            x = re.search(
+                                r"possibly compatible with ([\w_]+)", line)
+                            if x:
+                                if x.group(1) not in categories:
+                                    categories.append(x.group(1))
+                        break
+
+                # print out categories
+                option_exploit_dict = {}
+                counter = 1
+                for category in categories:
+                    print('   ' + category)
+                    print('   '+'='*len(category))
+                    filenames = os.listdir("exploits/"+category)
+                    for filename in filenames:
+                        if filename != "__init__.py" and filename != "__pycache__":
+                            if filename != "dos.py":
+                                option_exploit_dict[counter] = filename
+                                print(str(counter) + '. ' + filename)
+                                counter += 1
+                    print()
+
+                # ask for option of exploit
+                option = input("Please enter choice of exploit: ")
+
+                # run exploit
+                exploit_selected = option_exploit_dict.get(int(option))
+                exploit_selected = exploit_selected.replace(".py", "")
+                exploit_status = eval(exploit_selected)(exploit_ip)
+
+                # set exploit status in report_list
+                set_exploit_status(exploit_ip, exploit_status)
+                print(exploit_status)
+
+                global user_option
+
+                while True:
+                    user_option = input("Would you like to exploit another device? (y/n): ")
+
+                    if (user_option == 'y'):
+                        break
+
+                    elif (user_option == 'n'):
+                        break
 
                     else:
-                        port = row[key]
-                        print('   %s = %s' % (key, row[key]))
+                        print("Please input a valid option")
+                        continue
 
-                        for text in final_list:
-                            if (' ' + ip + ' ') in text:
-                                if (' ' + port + ' ') in text:
-                                    print('   ' + text)
+                if (user_option == 'n'):
+                    # delete the "temporary" scan-result text file
+                    textFilePath = masscan_output_dir + scanner.get_outfile()
+                    os.remove(textFilePath)
 
-                previous_ip = row[0]
-
-            print('\nTotal result: '+str(counter))
-
-            while True:
-                # ask for ip to exploit
-                exploit_ip = input("\nPlease enter the IP address to exploit: ")
-                regex = '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
-                r = re.compile(regex)
-                if r.match(exploit_ip):
-                    break
-                else:
-                    print("Invalid IP address! ")
-
-            print(exploit_ip + " may be compatible with the following exploits: \n")
-
-            # try to get categories of exploits selected ip may be compatible with
-            categories = []
-            for ip in report_list:
-                if ip['IP'][0] == exploit_ip:
-                    for line in ip['Banner']:
-                        x = re.search(
-                            r"possibly compatible with ([\w_]+)", line)
-                        if x:
-                            if x.group(1) not in categories:
-                                categories.append(x.group(1))
-                    break
-
-            # print out categories
-            option_exploit_dict = {}
-            counter = 1
-            for category in categories:
-                print('   ' + category)
-                print('   '+'='*len(category))
-                filenames = os.listdir("exploits/"+category)
-                for filename in filenames:
-                    if filename != "__init__.py" and filename != "__pycache__":
-                        if filename != "dos.py":
-                            option_exploit_dict[counter] = filename
-                            print(str(counter) + '. ' + filename)
-                            counter += 1
-                print()
-
-            # ask for option of exploit
-            option = input("Please enter choice of exploit: ")
-
-            # run exploit
-            exploit_selected = option_exploit_dict.get(int(option))
-            exploit_selected = exploit_selected.replace(".py", "")
-            exploit_status = eval(exploit_selected)(exploit_ip)
-
-            # set exploit status in report_list
-            set_exploit_status(exploit_ip, exploit_status)
-            print(exploit_status)
-
-            global user_option
-
-            while True:
-                user_option = input("Would you like to exploit another device? (y/n): ")
-
-                if (user_option == 'y'):
-                    break
-
-                elif (user_option == 'n'):
-                    break
+                    # generate HTML report
+                    create_report(report_list, masscan_output_dir, masscan_file_prefix, template_name)
+                    print('\n' + 'Program exiting...')
+                    sys.exit(0)
 
                 else:
-                    print("Please input a valid option")
                     continue
+        except KeyboardInterrupt:
+            # delete the "temporary" scan-result text file
+            textFilePath = masscan_output_dir + scanner.get_outfile()
+            os.remove(textFilePath)
 
-            if (user_option == 'n'):
-                sys.exit(0)
-
-            else:
-                continue
+            # generate HTML report
+            create_report(report_list, masscan_output_dir, masscan_file_prefix, template_name)
+            print('\n' + 'Program exiting...')
+            sys.exit(0)
             
     elif (choice == '2'):
-        print("help")
+        run = True
+        template_name = 'post_exploitation_scan_report_template.html'
+        while run:
+            try:
+                # request user for the filepath for the baseline HTML report 
+                htmlFilePath = input("Enter the file path for the baseline HTML report: ")
+
+                # retrieve the html file content
+                with open(htmlFilePath, 'r') as file:
+                    html = file.read()
+
+                # Initialising list of dictionary for baseline_list
+                baseline_list = []
+                dict_keys = ["IP", "Port"]
+                baseline_dict = {key: [] for key in dict_keys}
+
+                # Parse the html content and retrieve the first table
+                soup = BeautifulSoup(html, 'html.parser')
+                table = soup.find_all('table')[0]
+
+                #  Populate baseline_list
+                for row in table.find_all('tr')[1:]:                  
+                    cells = row.find_all('td')
+                    baseline_dict["IP"].append(cells[1].get_text())
+                    baseline_dict["Port"] = list(cells[2].get_text().split(","))
+                    baseline_dict_copy = baseline_dict.copy()
+                    baseline_list.append(baseline_dict_copy)
+                    
+                    # have to reset baseline_dict for the next device
+                    baseline_dict = {key: [] for key in dict_keys}
+                # print(baseline_list)
+
+                # run masscan
+                print("\nPlease enter the following to run masscan.")
+                masscan_file_prefix = input(
+                    "Enter the Prefix for the Post-Exploitation Report file (default = 'post-exploitation-scan-') : ") or "post-exploitation-scan-"
+                masscan_binary_path = input(
+                    "Enter Masscan application path (default = 'masscan') : ") or "masscan"
+                masscan_max_rate = input(
+                    "Masscan max rate in pps (default = 100) : ") or "100"
+                masscan_wait_time = input(
+                    "Masscan wait time (default = 30) : ") or "30"
+                masscan_output_dir = input(
+                    "Directory for the Post-Exploitation output files (default = 'post-exploitation-results/') : ") or "post-exploitation-results/"
+                if not os.path.exists(masscan_output_dir):
+                    os.makedirs(masscan_output_dir)
+                ip_target_range = input("Enter IP range with CIDR : ")
+                scanner = Masscan_Scanner.Masscan(target=ip_target_range,
+                                                prefix=masscan_file_prefix,
+                                                binary=masscan_binary_path,
+                                                max_rate=masscan_max_rate,
+                                                outdir=masscan_output_dir,
+                                                wait_time=masscan_wait_time)
+                ip_validity = scanner.check_ip_format(ip_target_range)
+                while(ip_validity == False):
+                    ip_target_range = input("Enter IP range with CIDR : ")
+                    ip_validity = scanner.check_ip_format(ip_target_range)
+                scanner.check_binary()
+                scanner.check_system()
+                scanner.run()
+                scanner.cleanup()
+
+                # parsing masscan output
+                parser = Masscan_Parser_Class.Masscan_Parser(
+                    file=masscan_output_dir+scanner.get_outfile())
+                parsed_list = parser.parse()
+                logging.info('Inserting data into scan DB...')
+                back_to_user()
+                db = DataBase_Class.Database()
+                tab_name = scanner.get_outfile().strip('.txt').replace('-', '_')
+                db.create_scan_table(tab_name)
+                db.insert_data(tab_name, parsed_list)
+                rows = db.extract_dist_ip(tab_name)
+                rows_2 = db.extract_first_ip(tab_name)
+                rows_3 = db.extract_last_ip(tab_name)
+
+                # store the result in a post_exploitation_list
+                post_exploitation_list = []
+                post_exploitation_dict = {key: [] for key in dict_keys}
+                
+                # Initialising variables
+                last_ip = ''
+                last_port = ''
+                first_ip = ''
+
+                # Obtain first ip in table
+                for row in rows_2:
+                    first_ip = row[0]
+
+                # Obtain last ip in the table
+                for row in rows_3:
+                    last_ip = row[0]
+                    last_port = row[1]
+                    
+                # Append the first IP
+                post_exploitation_dict["IP"].append(first_ip)
+
+                counter = 1
+                previous_ip = ''
+
+                # Append to post_exploitation_list through iterating the db
+                for row in rows:
+                    ip = ''
+                    port = ''
+
+                    for key in row.keys():
+                        if key == 'IP':
+                            ip = row[key]
+                            if ip == first_ip:
+                                pass
+
+                            elif ip != previous_ip:
+                                # Appends the dictionary to list
+                                post_exploitation_dict_copy = post_exploitation_dict.copy()
+                                post_exploitation_list.append(post_exploitation_dict_copy)
+
+                                # Reset dict key values
+                                post_exploitation_dict = {key: [] for key in dict_keys}
+                                post_exploitation_dict["IP"].append(ip)
+
+                        else:
+                            port = row[key]
+                            post_exploitation_dict["Port"].append(port)
+
+                    if last_ip == ip and last_port == port:
+                        post_exploitation_dict_copy = post_exploitation_dict.copy()
+                        post_exploitation_list.append(post_exploitation_dict_copy)
+
+                    previous_ip = row[0]
+                # print(post_exploitation_list)
+
+                # compare post_exploitation_list against baseline_list, & store the results in report_list (which will be used in report generation)
+                report_list = []
+                report_dict = {key: [] for key in dict_keys}
+                for baseline_device in baseline_list:
+                    for post_exploitation_device in post_exploitation_list:
+                        if baseline_device['IP'][0] == post_exploitation_device['IP'][0]:
+                            new_ports = list(set(post_exploitation_device['Port']).difference(baseline_device['Port']))
+                            if len(new_ports) != 0:
+                                report_dict['IP'] = baseline_device['IP']
+                                report_dict['Port'] = new_ports
+                                report_dict_copy = report_dict.copy()
+                                report_list.append(report_dict_copy)
+                # print(report_list)
+                
+                run = False
+            except OSError as e:
+                print(e)
+                run = True
+        
+        # delete the "temporary" scan-result text file
+        textFilePath = masscan_output_dir + scanner.get_outfile()
+        os.remove(textFilePath)
+        
+        # generate post exploitation HTML report
+        create_report(report_list, masscan_output_dir, masscan_file_prefix, template_name)
+        print('\n' + 'Program exiting...')
+        sys.exit(0)
 
     elif (choice == '3'):
         print("Exiting...")
